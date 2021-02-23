6250a53916a2b69cb7cf458641130bc8cdef96cd
Gustavo Ridgue <guferrari007@gmail.com>
2021-02-23 15:11:19 -0300

    Implemente project details and improve code

--- src/main/java/br/com/omni/projetos/controller/HomeController.java
+++ src/main/java/br/com/omni/projetos/controller/HomeController.java
@@ -10,12 +10,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("home")
@@ -29,10 +31,19 @@ public class HomeController {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @GetMapping
-    public String home(Model model, Principal principal) {
+    public String home(Model model) {
         List<Projeto>      projetos      = projetoRepositoy.findAll();
         model.addAttribute("projetos", projetos);
 
         return "home";
+    }
+
+    @GetMapping("/detalhes/{id}")
+    public String detalhe(@PathVariable("id") Long id, Model model) {
+        Optional<Projeto> projeto = projetoRepositoy.findById(id);
+        Projeto proj = projeto.get();
+        model.addAttribute("projeto", proj);
+
+        return "projeto/detalhes";
     }
 }
